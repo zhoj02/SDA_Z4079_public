02b738de08651c67bd6c0befacaddf1a406b9d6e
Jan Zhouf <48521182+zhoj02@users.noreply.github.com>
2025-03-05 18:57:02 +0100

    Update cviceni_sql_alchemy_cviceni_2.py

--- cviceni_sql_alchemy_cviceni_2.py
+++ cviceni_sql_alchemy_cviceni_2.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, Integer, Date
 from sqlalchemy.orm import sessionmaker
 
