502d393912de7c48e8a89e307d388a4e40afcea3
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-06 18:54:51 +0100

    March 6 - cviceni alchemy vztahy 4 - zacatek 3

--- march_6/cviceni_alchemy_vztahy_4.py
+++ march_6/cviceni_alchemy_vztahy_4.py
@@ -70,6 +70,9 @@ for client in result:
         print(booking.car)
 
 #2
+result = session.query(Bookings).filter_by(client_id=5)
+for booking in result:
+    print(booking.car)
 
 # conn = eng.connect()
 # s = select(Bookings).where(Bookings.client_id==3)
