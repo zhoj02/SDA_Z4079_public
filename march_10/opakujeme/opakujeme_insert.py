ee9f8af427101344a8b393de9538c5a952f9e867
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-10 18:46:12 +0100

    March 10 - opakujeme

--- march_10/opakujeme/opakujeme_insert.py
+++ march_10/opakujeme/opakujeme_insert.py
@@ -1,1 +1,7 @@
 # insert 3 klienty
+
+from mysql.connector import connect
+
+with connect(user="root", password="YourNewPassword", database="car_rental") as conn:
+    with conn.cursor() as cursor:
+        ...
