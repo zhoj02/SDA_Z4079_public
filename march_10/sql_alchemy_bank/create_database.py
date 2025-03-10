5ca51f4c3f591f71bbdd85b1ca5c28cd79d086fc
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-10 19:38:34 +0100

    March 10 - opakujeme

--- march_10/sql_alchemy_bank/create_database.py
+++ march_10/sql_alchemy_bank/create_database.py
@@ -1,4 +1,9 @@
-from sqlalchemy import create_engine
-
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
 eng = create_engine(f'mysql://root:YourNewPassword@localhost:3306/')
+
+Session = sessionmaker(bind=eng)
+session = Session()
+
+result = session.execute(text())
