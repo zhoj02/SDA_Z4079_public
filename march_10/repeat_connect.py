e1ff5a2372e466cef45f088356716e887da3fafe
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-10 17:56:28 +0100

    March 10 - connect test

--- march_10/repeat_connect.py
+++ march_10/repeat_connect.py
@@ -1,3 +1,9 @@
 from mysql.connector import connect
 
 conn = connect(host="localhost", user="root", password="YourNewPassword")
+test_select = "SELECT 1;"
+
+cursor = conn.cursor()
+
+cursor.execute(test_select)
+print(cursor.fetchall())
