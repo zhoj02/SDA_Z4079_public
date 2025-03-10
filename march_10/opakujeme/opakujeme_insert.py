cf349ddc2e3965e93328fdc9cfd0fe2ffc01318f
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-10 18:54:43 +0100

    March 10 - opakujeme

--- march_10/opakujeme/opakujeme_insert.py
+++ march_10/opakujeme/opakujeme_insert.py
@@ -1,7 +1,22 @@
 # insert 3 klienty
-
+# [('client_id', 'int', 'NO', 'PRI', None, 'auto_increment'),
+# ('name', 'varchar(30)', 'NO', '', None, ''),
+# ('surname', 'varchar(30)', 'NO', '', None, ''),
+# ('address', 'varchar(30)', 'NO', '', None, ''),
+# ('city', 'varchar(30)', 'NO', '', None, '')]
 from mysql.connector import connect
 
 with connect(user="root", password="YourNewPassword", database="car_rental") as conn:
     with conn.cursor() as cursor:
-        ...
+        cursor.execute("DESCRIBE clients")
+        print(cursor.fetchall())
+
+with connect(user="root", password="YourNewPassword", database="car_rental") as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("""
+        INSERT INTO clients (name, surname, address, city)
+        VALUES 
+        ('jan', 'pavel', 'Sokolovská','Prague'),
+        ('maria', 'majerova', 'Sokolovská','Brno'),
+        ('jan', 'honzik', 'Sokolovská','Ostrava')
+        """)
