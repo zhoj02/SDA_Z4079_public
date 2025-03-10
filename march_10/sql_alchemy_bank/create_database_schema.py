c7dd3274cf2e8b3abbbc3560dedc656ecbbbb845
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-10 20:10:17 +0100

    March 10 - opakujeme

--- march_10/sql_alchemy_bank/create_database_schema.py
+++ march_10/sql_alchemy_bank/create_database_schema.py
@@ -1,6 +1,6 @@
 # Cilem je vytvorit 3 tabulky banky
 # 1. Tabulka - Clienti - id, jmeno, prijmeni, adresu, datum narozeni
 # 2. Ucet - id, cislo_uctu, druh_uctu
-# 3. Tabulka - Transakce - id, cislo_uctu, castka, cas transakce, datum transakce
+# 3. Tabulka - Transakce - id, cislo_uctu_odchozi,  cislo_uctu_prichozi, castka, cas transakce, datum transakce
 
 Base.metadata.create_all(eng)
