6df53592cfdaaf755b161fba5117f1cdbebcc4e2
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-10 18:23:06 +0100

    March 10 - opakujeme

--- march_10/opakujeme_zobraz_vsechny_tabulky.py
+++ march_10/opakujeme_zobraz_vsechny_tabulky.py
@@ -1,3 +1,10 @@
 # Dejte print vsech tabulek v databazi car_rental
 # Databazi definujte v connect funkci
 # prikaz na to je: SHOW TABLES
+
+from mysql.connector import connect
+
+with connect(user="root", password="YourNewPassword", database="car_rental") as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SHOW TABLES")
+        print(cursor.fetchall())
