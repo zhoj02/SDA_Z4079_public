cc8eb2e80465c007bdd747280fecf9a31e4f37f4
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-10 18:37:04 +0100

    March 10 - opakujeme

--- march_10/opakujeme/opakujeme_select.py
+++ march_10/opakujeme/opakujeme_select.py
@@ -1,1 +1,8 @@
 # Vyber bookings, ktere maji vyssi hodnotu nez 500
+
+from mysql.connector import connect
+
+with connect(user="root", password="YourNewPassword", database="car_rental") as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM bookings WHERE total_amount > 500")
+        print(cursor.fetchall())
