f5d7e21a5c12a27466559cfaa1bee5b495e0938a
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-11 18:03:47 +0100

    Added drop all tables

--- march_10/sql_alchemy_bank/create_database_schema.py
+++ march_10/sql_alchemy_bank/create_database_schema.py
@@ -26,7 +26,26 @@ class Transaction(base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     acc_no_out = Column(String(20))
     acc_no_inc = Column(String(20))
-    datum = Column(DateTime, default=datetime.now)
+    amount = Column(Float)
+    date = Column(DateTime, default=datetime.now)
+
+
+class Client:
+    # 1. Tabulka - Clienti - id, jmeno, prijmeni, adresu, datum narozeni
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(20))
+    surname = Column(String(20))
+    address = Column(String(20))
+    birth_date = Column(Date)
+
+
+class Accounts:
+    # 2. Ucet - id, cislo_uctu, druh_uctu
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    cislo_uctu = Column(String(20))
+    druh_uctu = Column(String(20))
 
 
 base.metadata.drop_all(eng)
